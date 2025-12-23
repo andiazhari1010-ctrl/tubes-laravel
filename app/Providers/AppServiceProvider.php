1962b3e448b24d2b939a46fb1fec9d4408f69174
Andy Azhari Pane <andiazhari1010@gmail.com>
2025-12-23 09:14:10 +0700

    deploy code pt 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <--- JANGAN LUPA INI
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Paksa HTTPS jika aplikasi sedang online (Production)
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
