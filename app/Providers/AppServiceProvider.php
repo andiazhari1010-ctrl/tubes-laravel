f2550bba8f2ffe155e9e3009877c61c2a917133f
Andy Azhari Pane <andiazhari1010@gmail.com>
2025-12-23 09:28:03 +0700

    reset provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL; // <--- PASTIKAN BARIS INI ADA!
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,9 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Paksa HTTPS saat sudah online
-        if($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
+        //
     }
 }
